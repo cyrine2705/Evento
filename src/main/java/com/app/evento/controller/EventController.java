c66994d265fe722ddb75a52eaab656e0e5f197c8
Cyrine Srairi <sirine.srairi10@gmail.com>
2024-03-20 23:53:35 +0100

    added cross origin to all controllers

--- src/main/java/com/app/evento/controller/EventController.java
+++ src/main/java/com/app/evento/controller/EventController.java
@@ -8,7 +8,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin("*")
 @RestController
 @Slf4j
 @RequestMapping("api/events")
--- src/main/java/com/app/evento/controller/TestController.java
+++ src/main/java/com/app/evento/controller/TestController.java
@@ -4,13 +4,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api")
 @SecurityRequirement(name = "jwtEvento") //maaneha stockage ysir localemnt o tabaa swagger annotation hedhi
